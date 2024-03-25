f0dac2b72e556445cf1fb9d350569f899ba5999c
Mathys <mathys.ledanois@viacesi.fr>
2024-03-25 09:34:49 +0100

    first commit

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{ 
+    static void Square(int a , int b)
+    { 
+        a = a + b;
+        b += 1;
+        a = a * b;
+        b *=b;
+        Console.WriteLine(a + " " + b);
+    
+    }
+
+}
