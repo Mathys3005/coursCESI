4de324fa924029dfaf217ce225cf045fec5fae71
Mathys <mathys.ledanois@viacesi.fr>
2024-03-25 09:59:25 +0100

    coucou

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,22 +1,19 @@
 ﻿class Program
 { 
-    /*static void Square(int a , int b)
+    static void Square(int a , int b)
     { 
-        a = a + b;
-        b += 1;
-        a = a * b;
-        b *=b;
-        Console.WriteLine(a + " " + b);
+        a = a * a;
+        b = b * b;
+        Console.WriteLine("Carré de a :" + a + " " + "Carré de b :" + b);
     
-    }*/
+    }
     static void Main(string[] args) {
         int num1 = 5;
         int num2 = 10;
         Console.WriteLine(num1 + " " + num2);
         Square(num1, num2);
         Console.WriteLine(num1 + " " + num2);
-        Console.WriteLine("Press enter to Exit...")
-        Console.BackgroundColor = ConsoleColor.Green;
+        Console.WriteLine("Press enter to Exit...");
         Console.ReadLine();
     }
 }
