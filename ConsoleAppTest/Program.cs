de6de5e119600204550d7c71678e083d929a6144
Mathys <mathys.ledanois@viacesi.fr>
2024-03-27 00:15:28 +0100

    creation program

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] dogs = new string[] { "Thomas", "Snow", "Ali", "Axel", "Harry" };
+        IEnumerable<string> ExceptSnow =
+            from dog in dogs
+            where dog != "Snow"
+            select dog;
+        foreach (string dog in ExceptSnow)
+        {
+            Console.WriteLine(dog);
+        }
+    }
+}
